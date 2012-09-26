49f5adc99d79c0fe083fd8b2abdcdc69dd23ca28
Jamie Rumbelow <jamie@jamierumbelow.net>
2012-09-26 18:02:29 +0100

    Adding basic test scaffolding

---
+++ tests/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Sherlock - Asset pipelining so simple, it's elementary
+ *
+ * @link https://github.com/jamierumbelow/sherlock
+ * @copyright Copyright (c) 2012, Jamie Rumbelow <http://jamierumbelow.net>
+ */
+
+// Strap dat boot
